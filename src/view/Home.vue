79cd6901ba51d415aaa7a5d78f5640543570c19d
se.belov <cool.gsmes@yandex.ru>
2024-03-18 17:31:26 +0300

    Подключил API, лень проксировать ключ

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import axios from "../services/api";
 import weatherForecast from "../components/WeatherForecast.vue";
 import weatherInfo from "../components/WeatherInfo.vue";
 import sunriseSunset from "../components/SunriseSunset.vue";
@@ -24,9 +25,33 @@ export default {
     sunriseSunset,
     weatherByDay
   },
+  data() {
+    return {
+      weatherData: null,
+    };
+  },
+  mounted() {
+    this.fetchWeatherData();
+  },
+  methods: {
+    async fetchWeatherData() {
+      try {
+        const response = await axios.get('/current.json', {
+          params: {
+            key: 'e2f2fbc748174eed98c141646241803',
+            q: 'London',
+          },
+        });
+        this.weatherData = response.data;
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    },
+  },
 }
 </script>
 
 <style scoped lang="scss">
+
 
 </style>
